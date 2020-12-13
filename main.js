6170d9d085af54049249a42c5eaf2a505e49ccb1
evannor <erica.vannorman@gmail.com>
2020-12-13 12:00:07 -0800

    Style buttons

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // alert("The JavaScript is connected!");
 
 // Create the rest of the calculator buttons
-let buttonArray = ["clear", "()", "%", "/", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="];
+let buttonArray = ["Clear", "( )", "%", "/", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="];
 
 for(i = 0; i < buttonArray.length; i++) {
   let btn = document.createElement("button");
