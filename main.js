7bca76f997bf28a6e4bc952b1df78f26b11b4562
evannor <erica.vannorman@gmail.com>
2020-12-13 12:40:58 -0800

    Add event listener to all buttons

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-// alert("The JavaScript is connected!");
+// Create global input variable
+let inputValue;
 
-// Create the rest of the calculator buttons
+// Create calculator buttons
 let buttonArray = ["Clear", "( )", "%", "/", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="];
-
 for(i = 0; i < buttonArray.length; i++) {
   let btn = document.createElement("button");
   btn.innerText = buttonArray[i];
+  btn.id = buttonArray[i];
   let btnDiv = document.getElementById("btn-area");
   btnDiv.appendChild(btn);
+}
+
+// Button event listeners
+for (i = 0; i < buttonArray.length; i++) {
+  document.getElementById(buttonArray[i]).addEventListener("click", btnClick);
+}
+
+// Called once a button in clicked
+function btnClick() {
+  // Check for and save input
+  if (document.getElementById("screen").value === "0") {
+    inputValue = 0;
+  } else {
+    inputValue = document.getElementById("screen").value;
+  }
 }
