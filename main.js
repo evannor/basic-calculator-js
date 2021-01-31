0f1c5171517b632ab780e67c41e835039550ce02
evannor <erica.vannorman@gmail.com>
2021-01-31 11:14:34 -0800

    CE clears outputHistory, style clean up

--- main.js
+++ main.js
@@ -3,8 +3,29 @@ let inputValue;
 let outputFormula = "";
 
 // Create calculator buttons
-let buttonArray = ["Clear", "( )", "%", "/", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", "CE", "0", ".", "="];
-for(i = 0; i < buttonArray.length; i++) {
+let buttonArray = [
+  "Clear",
+  "( )",
+  "%",
+  "/",
+  "7",
+  "8",
+  "9",
+  "X",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "CE",
+  "0",
+  ".",
+  "=",
+];
+for (i = 0; i < buttonArray.length; i++) {
   let btn = document.createElement("button");
   btn.innerText = buttonArray[i];
   btn.id = buttonArray[i];
@@ -14,72 +35,82 @@ for(i = 0; i < buttonArray.length; i++) {
 
 // Button event listeners
 for (i = 0; i < buttonArray.length; i++) {
-  document.getElementById(buttonArray[i]).addEventListener("click", function(props){
-  let screenValue = document.getElementById("screen").value;
-  // Check for and save input
-  if (screenValue === "") {
-    // if outputFormula !== "", then complete arithmetic and set as input value --good use of anonymous arrow function
-    inputValue = 0;
-  } else {
-    inputValue = screenValue;
-  }
-
-  // Check which button is pressed
-  let btnPressed = props.target.id;
-  
-  // Button functionality
-  switch (btnPressed) {
-    case "Clear":
-      clearInput();
-      inputValue = 0;
-      break;
-    case "( )":
-      addParenthesis(inputValue);
-      console.log(outputFormula);
-      break;
-    case "%":
-      percentage(inputValue);
-      console.log(outputFormula);
-      break;
-    case "/":
-      arithmetic(inputValue, "/");
-      console.log(outputFormula);
-      break;
-    case "X":
-      arithmetic(inputValue, "X");
-      console.log(outputFormula);
-      break;
-    case "-":
-      arithmetic(inputValue, "-");
-      console.log(outputFormula);
-      break;
-    case "+":
-      arithmetic(inputValue, "+");
-      console.log(outputFormula);
-      break;
-    case "CE":
-      clearInput();
-      inputValue = 0;
-      outputFormula= "";
-      inputArray = [];
-      break;
-    case ".":
-      const decimalPlaces = prompt("How many digits to the right of the decimal?");
-      if (isNaN(decimalPlaces)) {
-        alert("Number of decimal places must be a number.");
-        break;
+  document
+    .getElementById(buttonArray[i])
+    .addEventListener("click", function (props) {
+      let screenValue = document.getElementById("screen").value;
+      // Check for and save input
+      if (screenValue === "") {
+        // if outputFormula !== "", then complete arithmetic and set as input value --good use of anonymous arrow function
+        inputValue = 0;
+      } else {
+        inputValue = screenValue;
       }
-      document.getElementById("screen").value = inputValue / Math.pow(10, decimalPlaces);
-      break;
-    case "=":
-      document.getElementById("screen").value = equal(inputValue, "=");
-      console.log(outputFormula += inputValue + " = " + document.getElementById("screen").value);
-      break;
-  
-    default: //gets called when only a number is pressed
-      inputValue = parseInt(btnPressed);
-      document.getElementById("screen").value += inputValue;
-      break;
-  }
-  });
+
+      // Check which button is pressed
+      let btnPressed = props.target.id;
+
+      // Button functionality
+      switch (btnPressed) {
+        case "Clear":
+          clearInput();
+          inputValue = 0;
+          break;
+        case "( )":
+          addParenthesis(inputValue);
+          console.log(outputFormula);
+          break;
+        case "%":
+          percentage(inputValue);
+          console.log(outputFormula);
+          break;
+        case "/":
+          arithmetic(inputValue, "/");
+          console.log(outputFormula);
+          break;
+        case "X":
+          arithmetic(inputValue, "X");
+          console.log(outputFormula);
+          break;
+        case "-":
+          arithmetic(inputValue, "-");
+          console.log(outputFormula);
+          break;
+        case "+":
+          arithmetic(inputValue, "+");
+          console.log(outputFormula);
+          break;
+        case "CE":
+          clearInput();
+          inputValue = 0;
+          outputFormula = "";
+          inputArray = [];
+          outputHistory.length = 0;
+          break;
+        case ".":
+          const decimalPlaces = prompt(
+            "How many digits to the right of the decimal?"
+          );
+          if (isNaN(decimalPlaces)) {
+            alert("Number of decimal places must be a number.");
+            break;
+          }
+          document.getElementById("screen").value =
+            inputValue / Math.pow(10, decimalPlaces);
+          break;
+        case "=":
+          document.getElementById("screen").value = equal(inputValue, "=");
+          console.log(
+            (outputFormula +=
+              inputValue + " = " + document.getElementById("screen").value)
+          );
+          break;
+
+        default:
+          //gets called when only a number is pressed
+          inputValue = parseInt(btnPressed);
+          document.getElementById("screen").value += inputValue;
+          break;
+      }
+    });
 }
